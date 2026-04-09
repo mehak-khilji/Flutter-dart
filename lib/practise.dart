6da75a0781db3b0040d3f2b8c5d1a0e51de36c38
mehakkhilji3 <mehakkhilji90@gmail.com>
2026-04-09 19:10:51 +0500

    var and dynamic

--- lib/practise.dart
+++ lib/practise.dart
@@ -1,39 +1,53 @@
-import 'dart:io';
+// import 'dart:io';
 
 void main() {
-  //   print("Welcome to Dart");
+  print("Welcome to Dart");
 
-  //     stdout.write("enter your name:");
-  //     var name = stdin.readLineSync();
-  //     print("welcomr, $name");
+  //       stdout.write("enter your name:");
+  //       var name = stdin.readLineSync();
+  //       print("welcomr, $name");
+  //     }
+
+  //     // declaration of variable
+
+  //     int b = 10;  // initialization
+  //     //assign
+
+  // void main() {
+  //   BigInt longvalue;
+  //   longvalue = BigInt.parse('123456789');
+  //   print(longvalue);
+
+  //   double percentage = 99.46;
+  //   print(percentage);
+
+  //   // num can be used to store value in integer as well as float
+  //   bool isLogin = false;
+  //   isLogin = true;
+  //   print(isLogin);
+  //     print(b);
+  //     b = 20;
+  //     print(b);
+
   //   }
+  // //inline declaration
+
+  // String name = "mehak";
+  // print ("name: $name");
 
   //   // human (); creating an class object
+  //     class Human {
+  //       Human();
 
-  //   class Human {
-  //     Human();
+  // variable and dynamic
+  string a = "mehak";
 
-  //   // declaration of variable
+  // var
+  dynamic section;
+  section = "A"; // string
+  section = 7; // integer
+  section = false; //boolean
 
-  //   int b = 10;  // initialization
-  //   //assign
-  BigInt longvalue;
-  longvalue = BigInt.parse('123456789');
-  print(longvalue);
-
-  double percentage = 99.46;
-  print(percentage);
-
-  // num can be used to store value in integer as well as float
-  bool isLogin = false;
-  isLogin = true;
-  print(isLogin);
-  //   print(b);
-  //   b = 20;
-  //   print(b);
+  var rollno = 11;
+  rollno = 18;
 }
-
-//inline declaration
-
-// String name = "mehak";
-// print (name);
