e4bd1ddd11bbf9eccfa62d802ad8dfc874a633cb
mehakkhilji3 <mehakkhilji90@gmail.com>
2026-04-14 12:04:48 +0500

    functions

--- lib/practise.dart
+++ lib/practise.dart
@@ -40,14 +40,38 @@ void main() {
   //       Human();
 
   // variable and dynamic
-  string a = "mehak";
+  // string a = "mehak";
 
   // var
-  dynamic section;
-  section = "A"; // string
-  section = 7; // integer
-  section = false; //boolean
+  // dynamic section;
+  // section = "A"; // string
+  // section = 7; // integer
+  // section = false; //boolean
 
-  var rollno = 11;
-  rollno = 18;
+  // var rollno = 11;
+  // rollno = 18;
+  var myC = myClass();
+
+  print(myC.Add(99, 10));
+
+  print(myC.Add(200, 300));
+
+  // myC.printName(); // function calling
+}
+
+class myClass {
+  myClass() {
+    // default constructor
+    print("myClass object created");
+  }
+
+  void printName() {
+    // Declaration
+    print("my name is Mehak"); //Definition
+  }
+
+  int Add(int no1, int no2) {
+    int sum = no1 + no2;
+    return sum;
+  }
 }
